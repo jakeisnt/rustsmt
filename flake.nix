233ae81e3476d586baef4e8df92e2ec17c85bc2b
Jacob Chvatal <jake@isnt.online>
2021-07-26 15:02:14 -0700

    use rust unstable toolchain

--- flake.nix
+++ flake.nix
@@ -4,20 +4,24 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        inherit (lib) attrValues;
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = nixpkgs.lib;
-        package = with pkgs; callPackage ./. { inherit pkgs; };
-      in {
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustup rustc z3 rustfmt rust-analyzer lld clang llvmPackages.libclang ];
-          LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
-        };
-        defaultPackage = package;
-      });
+  outputs = { self, nixpkgs, utils, rust-overlay, ... }:
+  utils.lib.eachDefaultSystem (system:
+  let
+    inherit (lib) attrValues;
+    overlays = [ (import rust-overlay) ];
+    pkgs = import nixpkgs {
+      inherit system overlays;
+    };
+    lib = nixpkgs.lib;
+    package = with pkgs; callPackage ./. { inherit pkgs; };
+  in {
+    devShell = with pkgs; mkShell {
+      buildInputs = [ z3 rustfmt rust-analyzer lld clang llvmPackages.libclang rust-bin.nightly.latest.default ];
+      LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+  };
+  defaultPackage = package;
+});
 }
